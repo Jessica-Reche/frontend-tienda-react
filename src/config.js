9c9ed5bf141ca301e235585a0dcbcd99d3fef3c5
Jessica <jessikareix20@gmail.com>
2023-04-25 00:11:50 +0200

    cambios

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 
 const config = {
     API_URL: process.env.NODE_ENV === 'production' ?  process.env.REACT_APP_API_URL_PROD : process.env.REACT_APP_API_URL_DEV,
-    STRIPE_API_KEY: 'pk_test_51MzSpXGewgqc010reXlgyGZzygjLjgCQgg0qxkKIveMu3G2ykU7oLfzwGBacK7wRJ81W5Eew71YrUNXN9rP7GGi800RzmRHxlC'
+    STRIPE_API_KEY: process.env.REACT_APP_STRIPE_APY_KEY_DEV
 };
 
 export default config;
